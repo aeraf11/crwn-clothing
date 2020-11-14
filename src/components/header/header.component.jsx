7ffb840e0e798c68b16db2106d795caf0268c2db
aeraf11 <aeraf11@gmail.com>
2020-11-14 17:24:23 +0000

    add redux, implemented user(reducer/actions) to header & app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../asset/crown.svg';
 import {auth } from '../../firebase/firebase.utils';
@@ -23,5 +24,9 @@ const Header = ({currentUser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
 
