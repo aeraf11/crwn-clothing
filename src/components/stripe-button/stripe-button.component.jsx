0431993023cf9e31e465742da5e04a463bdf2336
aeraf11 <aeraf11@gmail.com>
2020-11-16 20:14:21 +0000

    added stripe integration and updated favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HoDLqDrN9AxQRdqsIfL98tHtNuHBZDZ0u5trbM6MOy9fJZbpux29PzjTNq3aCXl8uub1lUw4dRgFVG2xEjIHF0q00LqFbChHh';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
